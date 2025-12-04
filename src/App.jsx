8270234504e763b865c88fd4c4654f699a2b415e
yyrrcc <music_v@naver.com>
2025-12-04 15:11:12 +0900

    axios, react-router-dom 설치

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  // 전역 변수 useContext - user (id 학번, userRole 권한)
+
   const [count, setCount] = useState(0);
 
   return (
