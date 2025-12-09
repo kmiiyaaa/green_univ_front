a3dabb7f449446ecd2bc45fd574d88caf855373f
kmiiyaaa <ckm1533@naver.com>
2025-12-09 09:07:31 +0900

    breakAppdetail 경로 추가

--- src/pages/user/info/StudentList.jsx
+++ src/pages/user/info/StudentList.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import api from '../../../api/httpClient';
 import DataTable from '../../../components/table/DataTable';
-import PaginationButton from '../../../components/form/PaginationButton';
 import InputForm from '../../../components/form/InputForm';
 import PaginationForm from '../../../components/form/PaginationForm';
 
