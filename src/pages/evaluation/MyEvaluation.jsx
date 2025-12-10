f9a28f26e8dbee8a37f0eb5bc33a145e55db8678
khyuna0 <khohyuna0102@gmail.com>
2025-12-10 14:25:26 +0900

    강의 평가 검색 기능 추가

--- src/pages/evaluation/MyEvaluation.jsx
+++ src/pages/evaluation/MyEvaluation.jsx
@@ -3,28 +3,32 @@ import api from '../../api/httpClient';
 import DataTable from '../../components/table/DataTable';
 
 export default function MyEvaluation() {
-	// 교수 - 내 강의 평가 조회 + 담당 강의 옵션으로 검색
+	// 교수 - 내 강의 평가 조회 + 담당 강의 옵션으로 검색 (페이징 x)
 	const [subName, setSubName] = useState(null); // 선택한 학과이름
 	const [evalList, setEvalList] = useState([]); // 강의평가 리스트
 	const [subNameList, setSubNameList] = useState([]); // 교수의 과목 리스트
 
-	useEffect(() => {
-		const loadMyEvaluation = async () => {
-			try {
-				let subject_name = null;
-				if (subName !== null) {
-					subject_name = subName;
-				}
-				const res = await api.get('/evaluation/read', {
-					subject_name,
-				});
-				setSubNameList(res.data.subNames);
-				setEvalList(res.data.eval);
-				console.log(res.data);
-			} catch (e) {
-				alert(e.response.data.message);
+	const loadMyEvaluation = async () => {
+		try {
+			// 검색 값 있을 때
+			let subject_Name = null;
+			if (subName !== null) {
+				subject_Name = subName;
 			}
-		};
+			const res = await api.get('/evaluation/read', {
+				params: {
+					subject_Name: subject_Name,
+				},
+			});
+			setSubNameList(res.data.subNames);
+			setEvalList(res.data.eval);
+		} catch (e) {
+			alert(e.response.data.message);
+		}
+	};
+
+	useEffect(() => {
+		// 처음 로딩
 		loadMyEvaluation();
 	}, []);
 
@@ -41,6 +45,13 @@ export default function MyEvaluation() {
 
 	return (
 		<div>
+			<select name="subName" value={subName ?? '과목 선택'} onChange={(e) => setSubName(e.target.value)}>
+				{subNameList.map((sub, idx) => (
+					<option key={idx}>{sub.name}</option>
+				))}
+			</select>
+			<button onClick={() => loadMyEvaluation()}>검색</button>
+
 			<DataTable headers={headers} data={tableData} />
 		</div>
 	);
