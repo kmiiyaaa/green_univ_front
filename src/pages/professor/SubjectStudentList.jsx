5b2f8d81a3bb548cbc3d26800c8ed0b223f3d584
khyuna0 <khohyuna0102@gmail.com>
2025-12-11 17:21:03 +0900

    성적 유효성 체크 중

--- src/pages/professor/SubjectStudentList.jsx
+++ src/pages/professor/SubjectStudentList.jsx
@@ -3,40 +3,51 @@ import api from '../../api/httpClient';
 import DataTable from '../../components/table/DataTable';
 import GradeInput from './GradeInput';
 
-export default function SubjectStudentList({ subjectId, subName }) {
+export default function SubjectStudentList({ subjectId, subName, setListOpen }) {
 	// 과목 별 수강 학생 조회
 	const [studentList, setStudentList] = useState([]);
 	const [stuNum, setStuNum] = useState(0); // 수강 학생 수
 	const [gradeitem, setGradeItem] = useState([]); // 점수기입용 데이터
 	const [openGrade, setOpenGrade] = useState(false); // 점수기입 화면 열기 여부
 
+	const loadStudentList = async () => {
+		try {
+			const res = await api.get(`/professor/subject/${subjectId}`);
+			console.log(res.data);
+			setStudentList(res.data.studentList);
+			setStuNum(res.data.stuNum);
+		} catch (e) {
+			console.error('수강 학생 조회 에러 : ', e);
+		}
+	};
 	useEffect(() => {
-		const loadStudentList = async () => {
-			try {
-				const res = await api.get(`/professor/subject/${subjectId}`);
-				console.log(res.data);
-				setStudentList(res.data.studentList);
-				setStuNum(res.data.stuNum);
-			} catch (e) {
-				console.error('수강 학생 조회 에러 : ', e);
-			}
-		};
 		loadStudentList();
 	}, [openGrade]);
 
-	// 점수 기입 컴포넌트 열기
+	// 점수 기입 컴포넌트 열기 (넘겨줄 props들)
 	const handleOpenGrade = useCallback(
 		(s) => {
 			setGradeItem([
 				{
-					...s, // ← 기존 학생 정보 그대로
-					subjectId: subjectId, // ← 과목 ID 추가
+					...s, // 기존 학생 정보
+					subjectId: subjectId, // 과목 ID 추가
 				},
 			]);
 			setOpenGrade(true);
 		},
 		[subjectId]
 	);
+
+	// 상대평가 과목 : 성적을 모두 기입한 경우, 전체 학생 등급 산출
+	const getRelativeGrade = async () => {
+		try {
+			await api.patch(`/professor/relativeGrade/${subjectId}`);
+		} catch (e) {
+			alert(e.response.data.message);
+			console.error('전체 학생 등급 산출 에러 : ', e);
+		}
+		loadStudentList();
+	};
 
 	// 테이블 데이터
 	const headers = [
@@ -83,6 +94,9 @@ export default function SubjectStudentList({ subjectId, subName }) {
 							<h4>
 								수강 인원 : {stuNum}명 ({stuNum < 20 ? '절대평가' : '상대평가'})
 							</h4>
+							{/* 상대평가 과목일 때, 전체 학생 등급 산출 */}
+							{stuNum >= 20 && <button onClick={() => getRelativeGrade()}>전체 학생 등급 산출</button>}
+							<button onClick={() => setListOpen()}>내 강의 목록</button>
 							<DataTable headers={headers} data={tableData} />
 						</div>
 					) : (
@@ -92,7 +106,7 @@ export default function SubjectStudentList({ subjectId, subName }) {
 			)}
 
 			{/* 학생 점수 기입 */}
-			{openGrade && <GradeInput gradeitem={gradeitem} setOpenGrade={setOpenGrade} />}
+			{openGrade && <GradeInput gradeitem={gradeitem} setOpenGrade={setOpenGrade} stuNum={stuNum} />}
 		</div>
 	);
 }
