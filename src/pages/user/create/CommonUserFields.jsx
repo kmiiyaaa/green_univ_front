635358268de56c277fc93f5cc5c9dca91062d15d
kmiiyaaa <ckm1533@naver.com>
2025-12-22 12:15:23 +0900

    배포 수정

--- src/pages/user/create/CommonUserFields.jsx
+++ src/pages/user/create/CommonUserFields.jsx
@@ -4,7 +4,7 @@ import '../../../assets/css/UserCreate.css';
 
 export default function CommonUserFields({ formData, onChange }) {
 	// 성별 옵션 정의
-	// 화면에 보일 글자는 label,  서버로 보낼 값은 value
+	// 화면에 보일 글자는 label, 서버로 보낼 값은 value
 	const genderOptions = [
 		{ label: '여성', value: '여성' },
 		{ label: '남성', value: '남성' },
