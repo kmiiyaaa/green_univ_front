8430b0a2ec0f41fd83facd7dc8dbb8cf1345899c
kmiiyaaa <ckm1533@naver.com>
2025-12-29 15:17:48 +0900

    경로 수정추가

--- src/api/httpClient.jsx
+++ src/api/httpClient.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 	//baseURL: 'http://localhost:8888/api',
-	baseURL: '/api',
+	baseURL: import.meta.env.VITE_API_BASE_URL ?? '/api',
 	withCredentials: true,
 });
 
